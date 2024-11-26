76e17b1df7e1f79066f281e3b74507a5ec564a0d
kliea <90135283+kliea@users.noreply.github.com>
2024-11-27 02:44:55 +0800

    Update AuthenticatedSessionController.php

--- PCA-PAYROLL/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ PCA-PAYROLL/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -36,6 +36,7 @@ class AuthenticatedSessionController extends Controller
         return redirect()->intended(route('bioadmin.dashboard'));
     }
 
+    
     /**
      * Destroy an authenticated session.
      */
